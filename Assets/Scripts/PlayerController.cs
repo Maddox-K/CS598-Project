62b297d7acb91ac7a66703fe4e706a5ec38d6635
Maddox Kafka <129709400+Maddox-K@users.noreply.github.com>
2024-09-19 17:59:40 -0500

    add idle player animations
    add sprites for each frame of directional idle animations; create animation subfolder of Assets folder; edit player movement script to set look animation variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public PlayerInputActions playerControls;
 
+    public Animator animator;
+
     Vector2 moveDirection = Vector2.zero;
+    Vector2 lookDirection = Vector2.zero;
     private InputAction move;
     private InputAction interact;
 
@@ -35,10 +38,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //movement
         moveDirection = move.ReadValue<Vector2>();
+
+        //animation
+        if(!Mathf.Approximately(moveDirection.x, 0.0f) || !Mathf.Approximately(moveDirection.y, 0.0f))
+        {
+            lookDirection.Set(moveDirection.x, moveDirection.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("look_x", lookDirection.x);
+        animator.SetFloat("look_y", lookDirection.y);
+        //animator.SetFloat("Speed", moveDirection.magnitude);
     }
 
     private void FixedUpdate() {
+        //movement (rigidbody physics compoenent)
         rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
     }
 }

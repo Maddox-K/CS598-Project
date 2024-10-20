a7cebc00309292de44138198d1990a3b4e93e11a
Maddox Kafka <129709400+Maddox-K@users.noreply.github.com>
2024-10-20 17:50:23 -0500

    Add to Encounter Scene
    Create tilemap in encounter scene; create wrench sprite and animate

--- Assets/BalladOfTheBog/Scripts/Objects/Projectile.cs
+++ Assets/BalladOfTheBog/Scripts/Objects/Projectile.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private Animator animator;
+    private Rigidbody2D rb;
     public int damage;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            animator.SetFloat("horizontal", rb.velocity.x);
+            animator.SetFloat("vertical", rb.velocity.y);
+        }
+    }
 }

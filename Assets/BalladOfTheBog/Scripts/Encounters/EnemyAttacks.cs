fbac0679f6e13dd5d1baa55936657f4f783dfc7e
Maddox Kafka <129709400+Maddox-K@users.noreply.github.com>
2025-02-14 20:29:17 -0600

    Create Bog Encounter and Edit EncounterManager
    Create and import tiles for bog encounter scenery; add int encounterscenery field to enemy attacks; edit EncounterManager to switch between the grids

--- Assets/BalladOfTheBog/Scripts/Encounters/EnemyAttacks.cs
+++ Assets/BalladOfTheBog/Scripts/Encounters/EnemyAttacks.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Encounter/New Enemy Attacks")]
 public class EnemyAttacks : ScriptableObject
 {
+    // encounterScenery: 0 -> city, 1 -> bog
+    public int encounterScenery;
     public Projectile projectile;
     public Vector2[] spawnPositions;
     public Vector2[] launchDirections;
